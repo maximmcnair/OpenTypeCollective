88f768fa9cf370bbe885442f8427357775d2b0eb
Maxim McNair <mail@maximmcnair.com>
2023-01-16 10:36:01 -0600

    header styling adjustments

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -6,8 +6,8 @@
 
 <header class:page-home={$page.route.id === '/'} class:page-system={$page.route.id === '/system'}>
 	<div class="header-content">
-		<a href="/" class:active={$page.route.id === '/'}>Typefaces</a>
-		<a href="/system" class:active={$page.route.id === '/system'}>System Creator</a>
+		<a href="/" class="header-page-home" class:active={$page.route.id === '/'}>Typefaces</a>
+		<a href="/system" class="header-page-system" class:active={$page.route.id === '/system'}>System Creator</a>
 	</div>
 </header>
 
@@ -22,7 +22,7 @@
 		border-radius: 100px;
 		transform: translate(-50%, 0px);
 		height: 70px;
-    width: 301px;
+    width: 311px;
 	}
 
   @media (min-width: 800px){
@@ -37,7 +37,7 @@
   }
 
 	.header-content {
-		margin: 10px;
+		margin: 10px 0px;
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
@@ -47,10 +47,14 @@
 	header a {
 		font-size: 18px;
 		font-family: var(--typeface-body);
+    font-weight: 600;
 		color: var(--color-white);
+    display: block;
 		transition: all 0.3s;
 		padding: 10px 15px;
 		z-index: 4;
+    text-align: center;
+    position: absolute;
 	}
 	header a:hover {
     text-decoration: none;
@@ -68,14 +72,24 @@
 		transition: left 0.3s;
 	}
 
+  .header-page-home {
+		left: 10px;
+		width: 130px;
+  }
+
+  .header-page-system {
+		width: 180px;
+		left: 130px;
+  }
+
 	header.page-home::after {
 		left: 10px;
-		width: 121px;
+		width: 130px;
 	}
 
 	header.page-system::after {
 		width: 160px;
-		left: 130px;
+		left: 140px;
 	}
 
 	a.active {
